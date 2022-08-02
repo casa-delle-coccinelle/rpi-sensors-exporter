adfc5c5d609065f7fb0eb62053e3291ba04d409b
aneliya-n-ivanova <aneliya.n.ivanova@gmail.com>
2022-08-02 17:09:35 +0300

    introducing main

--- rpi-sensors-exporter/exporter.py
+++ rpi-sensors-exporter/exporter.py
@@ -75,7 +75,10 @@ def getSensors():
     return make_wsgi_app()
 
 
-if __name__ == '__main__':
+def main():
     serve(app, host='0.0.0.0', port=(os.getenv('SENSORS_EXPORTER_PORT') or 8080))
 
+
+if __name__ == '__main__':
+    main()
 
