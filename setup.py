3d8cc015ed9811082a6c4391ae7468fd72cd56e3
aneliya-n-ivanova <aneliya.n.ivanova@gmail.com>
2022-08-23 13:38:26 +0300

    remove explicid package name

--- setup.py
+++ setup.py
@@ -32,6 +32,5 @@ setup(name='rpi_sensors_exporter',
       dependency_links=[
           'git+https://github.com/adafruit/Adafruit_Python_BMP.git#egg=Adafruit_BMP-1.5.4', 
           'git+https://github.com/casa-delle-coccinelle/Python_SI1145.git#egg=SI1145-1.0.0'
-          ],
-      packages=['rpi_sensors_exporter']
+          ]
      )
